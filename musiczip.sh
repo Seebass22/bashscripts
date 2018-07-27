8bb45d454f4390537b1c1bf4d24dabb0d4de6270
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2018-07-28 01:12:07 +0200

    added musiczip

---
+++ musiczip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+	echo "error"
+	exit -1
+fi
+
+file=$1
+newdir=${file%.zip}
+mkdir "$newdir"
+mv "$file" "$newdir"
+cd "$newdir"
+unzip "$file"
+mv "$file" ../
+cd ..
+mv "$newdir" music/
+
