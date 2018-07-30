8018c4f309400f1223d39ee072c066328ddaedca
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2018-07-31 00:08:14 +0200

    screenshot script now positions ws 10 correctly

--- i3screenshot.sh
+++ i3screenshot.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 mkdir -p /tmp/i3screenshot/
+rm /tmp/i3screenshot/*
 args=$(i3-msg -t get_workspaces | sed -e 's/,{/\n/g' | cut -d : -f 2 | cut -d , -f 1 | tr '\n' ' ')
 for ws in $args
 do
-	file=tmp$ws.png
+	file=${ws}tmp.png
 	i3-msg workspace $ws
 	sleep 0.2
 	scrot /tmp/i3screenshot/$file
 done
 
 pushd /tmp/i3screenshot/
-convert tmp*.png +append  result.png
-rm tmp*.png
+args=$(ls -l | awk '{print $9}' | sed 1d | sort -n | tr '\n' ' ')
+convert $args +append  result.png
 popd
 mv /tmp/i3screenshot/result.png ./
 
