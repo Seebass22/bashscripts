9e59ed689640ecb77daa7cb8c52422446a773aa8
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2018-08-04 20:46:34 +0200

    made quit script case insensitive

--- i3quit.sh
+++ i3quit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-prompt=$(printf "no\nyes" | dmenu -p "Do you really want to exit i3?")
+prompt=$(printf "no\nyes" | dmenu -i -p "Do you really want to exit i3?")
 if [ $prompt == "yes" ] ; then
 	i3-msg exit
 fi
