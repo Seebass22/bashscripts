4ad087c8b5685b1aba518498a4940f5afd2e51af
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2018-09-05 13:57:09 +0200

    updated books.sh to read multiple folders

---
+++ books.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ~/Documents/books/
+cd $(ls -d */ | dmenu -i -p "choose folder")
+book=$(ls *.pdf | dmenu -i -p "which book do you want to select?")
+if [ ! -z "$book" ]; then
+	zathura "$book" & disown
+fi
