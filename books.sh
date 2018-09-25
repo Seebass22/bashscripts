a54051f869a9a23b3604e5cefa6d87ea9cff2588
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2018-09-25 11:20:39 +0200

    fixed books.sh

--- books.sh
+++ books.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/Documents/books/
-cd $(ls -d */ | dmenu -i -p "choose folder")
+cd "$(ls -d */ | dmenu -i -p "choose folder")"
 book=$(ls *.pdf | dmenu -i -p "which book do you want to select?")
 if [ ! -z "$book" ]; then
 	zathura "$book" & disown
