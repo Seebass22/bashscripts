54c0a8b57a6ee66346782af4c47a2d2f77c5e93c
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2018-10-12 22:11:03 +0200

    fixed sed breaking symlinks

--- maketrans.sh
+++ maketrans.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i 's/^URxvt.transparent: false/!URxvt.transparent: false/' ~/.Xdefaults
-sed -i 's/^!URxvt.transparent: true/URxvt.transparent: true/' ~/.Xdefaults
+sed -i --follow-symlinks 's/^URxvt.transparent: false/!URxvt.transparent: false/' ~/.Xdefaults
+sed -i --follow-symlinks 's/^!URxvt.transparent: true/URxvt.transparent: true/' ~/.Xdefaults
--- shading.sh
+++ shading.sh
@@ -3,6 +3,6 @@ if [ $# -ne 1 ]
 then
 	cat ~/.Xdefaults | grep shading
 else
-	sed -iE "s/URxvt.shading: [0-9]*/URxvt.shading: $1/" ~/.Xdefaults
+	sed -iE --follow-symlinks "s/URxvt.shading: [0-9]*/URxvt.shading: $1/" ~/.Xdefaults
 	cat ~/.Xdefaults | grep shading
 fi
