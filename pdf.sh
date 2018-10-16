7a7f9f61320992dd25b70d4e35189c518ff833ab
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2018-10-16 22:26:28 +0200

    fixed error in pdf.sh

--- pdf.sh
+++ pdf.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-pdf=$(ls *.pdf | dmenu -i -p "which PDF do you want to select?")
-[ -z $pdf ] && echo "no PDFs found" && exit 1
+if ls | grep pdf > /dev/null ; then
+	pdf=$(ls *.pdf | dmenu -i -p "which PDF do you want to select?")
+fi
+[ -z "$pdf" ] && echo "no PDFs found" && exit 1
 zathura "$pdf" & disown
