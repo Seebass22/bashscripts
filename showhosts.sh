d3530f1fa29114f74e312ce181f6b1c8d7e0e9aa
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2018-12-22 15:26:16 +0100

    updated showhosts to use $1 as ip

--- showhosts.sh
+++ showhosts.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-nmap -v -sL 192.168.2.1/24 | awk '{print $5 $6}' | grep -v ^[0-9] | sed '1,4d' | head -n -1
+ip='192.168.2.1/24'
+if [ $# -eq 1 ]; then
+	ip="$1"
+fi
+nmap -v -sL $ip | awk '{print $5 $6}' | grep -v ^[0-9] | sed '1,4d' | head -n -1
