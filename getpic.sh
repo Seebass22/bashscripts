017fe5fe2bc8ad78f6a5d4e16f4040fd7d0d2987
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2019-02-09 21:41:22 +0100

    removed getpic.sh

--- getpic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#rm '~/Downloads/rpi.jpg'
-mv ~/Downloads/rpi.jpg ~/.trash/
-ssh rpi '~/Documents/bashscripts/test.sh'
-scp rpi:/home/pi/Pictures/temp/rpi.jpg /home/seb22/Downloads/
-feh ~/Downloads/rpi.jpg
