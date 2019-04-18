c673168e71c52d7104a32dfb4f7305022ae3e73d
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2019-04-18 23:39:03 +0200

    added stream video+chat script

---
+++ twitch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+url="$(xclip -selection clipboard -out)"
+notify-send "$url"
+
+site="$(echo $url | cut -d'.' -f2)"
+if [ "$site" == twitch ]; then
+	herbstclient load "$(cat ~/.config/herbstluftwm/layouts/twitch)"
+	channel="$(echo $url | cut -d'/' -f4)"
+	qutebrowser -R "https://www.twitch.tv/popout/$channel/chat?popout=" > /dev/null 2>&1 & disown
+	setsid mpv -quiet "$url" > /dev/null 2>&1 &
+	sleep 2;
+	herbstclient focus r
+elif [ "$site" == youtube ]; then
+	herbstclient load "$(cat ~/.config/herbstluftwm/layouts/twitch)"
+	id="$(echo $url | cut -d'?' -f2)"
+	qutebrowser -R "https://www.youtube.com/live_chat?$id&is_popout=1" > /dev/null 2>&1 & disown
+	setsid mpv -quiet "$url" > /dev/null 2>&1 &
+	sleep 2;
+	herbstclient focus r
+else
+	notify-send "invalid link"
+fi
