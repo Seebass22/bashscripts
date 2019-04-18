10548a2259e2187724d4fd0565e0bcd63ab4a092
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2019-04-18 23:45:44 +0200

    dmenu books script now lists items vertically

--- books.sh
+++ books.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/Documents/books/
-cd "$(ls -d */ | dmenu -i -p "choose folder")"
+cd "$(ls -d */ | dmenu -l 25 -i -p "choose folder")"
 book=$(ls *.pdf | dmenu -l 25 -i -p "which book do you want to select?")
 if [ ! -z "$book" ]; then
 	zathura "$book" --fork
