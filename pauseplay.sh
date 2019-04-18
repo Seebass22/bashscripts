e232a05fe50efd177623265e8f661d28134ba8d0
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2019-04-18 23:42:49 +0200

    spotify/mpd script now pauses mpd even if spotify is running

--- pauseplay.sh
+++ pauseplay.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-ps aux | grep "[s]potify"
+mpdstatus="$(mpc status | sed -n '2p' | awk '{print $1}')"
+
+ps aux | grep -q "[s]potify"
 if [ $? -eq 0 ]; then
-	dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
+	if [ "$mpdstatus" == '[playing]' ]; then
+		echo "$mpdstatus"
+		mpc pause
+	else
+		dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
+	fi
 elif [ $? -eq 1 ]; then
 	mpc toggle
 fi
