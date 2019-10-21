ab3915019bb9436c85f4c9d435c57a6f5bcc5dc4
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2019-10-21 14:53:45 +0200

    books script now also uses djvu files

--- books.sh
+++ books.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd ~/Documents/books/
 cd "$(ls -d */ | dmenu -l 25 -i -p "choose folder")"
-book=$(ls *.pdf | dmenu -l 25 -i -p "which book do you want to select?")
+djvus="$(echo *.djvu)"
+if [ "$djvus" == '*.djvu' ]; then
+	book=$(ls *.pdf | dmenu -l 25 -i -p "which book do you want to select?")
+else
+	book=$(ls *.pdf *.djvu | dmenu -l 25 -i -p "which book do you want to select?")
+fi
 if [ ! -z "$book" ]; then
 	zathura "$book" --fork
 fi
