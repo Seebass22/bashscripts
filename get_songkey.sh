bd7b85076614bcfd3327655dac42628109edf754
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2020-01-02 21:01:45 +0100

    added song key finder script

---
+++ get_songkey.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+mpdstatus="$(mpc status | sed -n '2p' | awk '{print $1}')"
+
+ps aux | grep -q "[s]potify"
+if [ $? -eq 0 ]; then
+	if [ "$mpdstatus" == '[playing]' ]; then
+		artist="$(mpc status | head -n1 | cut -d'-' -f1)"
+		title="$(mpc status | head -n1 | cut -d'-' -f2)"
+	else
+		title="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/title/{n;p}' | cut -d '"' -f 2)"
+		artist="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata | sed -n '/artist/{n;n;p}' | cut -d '"' -f 2)"
+	fi
+elif [ $? -eq 1 ]; then
+	artist="$(mpc status | head -n1 | cut -d'-' -f1)"
+	title="$(mpc status | head -n1 | cut -d'-' -f2)"
+fi
+
+artistp="$(echo $artist | sed 's/ /+/g')"
+titlep="$(echo $title | sed 's/ /+/g')"
+
+echo "$artist - $title"
+link="https://tunebat.com/Search?q=${artistp}+${titlep}"
+echo "$link"
+setsid firefox "$link" >/dev/null 2>&1 &
