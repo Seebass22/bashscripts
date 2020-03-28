d1bd12cd1cb2b287aa34e0dd312ed8cc09b666c0
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2020-03-28 17:23:28 +0100

    books.sh now reads epubs too

--- books.sh
+++ books.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd ~/Documents/books/
 cd "$(ls -d */ | dmenu -l 25 -i -p "choose folder")"
-djvus="$(echo *.djvu)"
-if [ "$djvus" == '*.djvu' ]; then
-	book=$(ls *.pdf | dmenu -l 25 -i -p "which book do you want to select?")
-else
-	book=$(ls *.pdf *.djvu | dmenu -l 25 -i -p "which book do you want to select?")
+book="$(find . -maxdepth 1 -iregex '.*\.pdf\|.*\.epub\|.*\.djvu' | sed 's/^..//' | dmenu -l 25 -i -p "which book do you want to select?")"
+if [[ $book =~ .*\.epub ]]; then
+	ebook-viewer "$book" & disown
+	exit
 fi
+
 if [ ! -z "$book" ]; then
 	zathura "$book" --fork
 fi
