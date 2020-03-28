ee443a63bf060279d3b008febe061369e34d5753
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2020-03-28 17:38:46 +0100

    books.sh can show all books

--- books.sh
+++ books.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+openbook () {
+	if [[ $book =~ .*\.epub ]]; then
+		ebook-viewer "$book" & disown
+		exit
+	fi
+
+	if [ ! -z "$book" ]; then
+		zathura "$book" --fork
+	fi
+}
+
 cd ~/Documents/books/
 cd "$(ls -d */ | dmenu -l 25 -i -p "choose folder")"
-book="$(find . -maxdepth 1 -iregex '.*\.pdf\|.*\.epub\|.*\.djvu' | sed 's/^..//' | dmenu -l 25 -i -p "which book do you want to select?")"
-if [[ $book =~ .*\.epub ]]; then
-	ebook-viewer "$book" & disown
+
+#all books
+if [ "$(pwd -P)" = /home/seb22/Documents/books/all ]; then
+	cd ..
+	book="$(find . -iregex '.*\.pdf\|.*\.epub\|.*\.djvu' | sed 's/^..//' | grep -v '^epub' | dmenu -l 25 -i -p "which book do you want to select?")"
+	echo all
+	openbook
 	exit
 fi
 
-if [ ! -z "$book" ]; then
-	zathura "$book" --fork
-fi
+#in specific folder
+book="$(find . -maxdepth 1 -iregex '.*\.pdf\|.*\.epub\|.*\.djvu' | sed 's/^..//' | dmenu -l 25 -i -p "which book do you want to select?")"
+openbook
