4b521e4672bc5e7744f5d8638690d6bef1549dd5
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2020-12-21 01:43:27 +0100

    move old unused scripts to separate dir

--- old/oggify.sh
+++ old/oggify.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# convert audio file to mkv with opus codec - very tiny file size
 [ $# -ne 1 ] || [[ ! $1 =~ .+\.+ ]] && exit
 ffmpeg -i "$1" -c:a libopus -b:a 32k "${1%.*}.mkv"
--- old/showfilesize.sh
+++ old/showfilesize.sh
@@ -1,3 +1,2 @@
-
 #!/bin/bash
 ls -l | awk '{sum += $5} END {print sum/1048576}'
