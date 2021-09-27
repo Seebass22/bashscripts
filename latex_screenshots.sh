ad83ccff3178b73aad1aaba176476854abb1a812
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-09-27 18:31:19 +0200

    add arguments to latex history script

--- latex_screenshots.sh
+++ latex_screenshots.sh
@@ -17,23 +17,53 @@ summarize(){
 }
 
 convert_to_png(){
-	convert -background white -alpha remove -alpha off -density $density "$1" ${1%.pdf}.png
+	convert -background white -alpha remove -alpha off -density $density "$1" "${1%.pdf}.png"
 }
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+	key="$1"
+
+	case $key in
+		-i|--input)
+			input="$2"
+			shift # past argument
+			shift # past value
+			;;
+		-d|--density)
+			density="$2"
+			shift # past argument
+			shift # past value
+			;;
+		-l|--pagelayout)
+			pagelayout="$2"
+			shift # past argument
+			shift # past value
+			;;
+		-o|--outputdir)
+			outputdir="$2"
+			shift # past argument
+			shift # past value
+			;;
+		*)    # unknown option
+			POSITIONAL+=("$1") # save it in an array for later
+			shift # past argument
+			;;
+	esac
+done
 
 i=1
 mkdir -p "$outputdir"
-if [ $# -eq 1 ]; then
-	input="$1"
-else
+if [ -z "$input" ]; then
 	input="$(echo *.tex)"
 fi
 
 while true; do
 	filename="$(printf '%03d' $i).pdf"
 	compile "$input"
-	mv ${input%.tex}.pdf $filename
-	summarize $filename
-	mv ${filename%.pdf}_summary.pdf "$outputdir"
+	mv "${input%.tex}.pdf" "$filename"
+	summarize "$filename"
+	mv "${filename%.pdf}_summary.pdf" "$outputdir"
 
 	# checkout parent commit if exists
 	parent="$(git rev-list --topo-order HEAD..main | tail -1)"
