23301e91679a53db023b601b17c664ef78f240ca
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2022-05-06 17:27:01 +0200

    add resolution options to vertical_blur.sh

--- ffmpeg/vertical_blur.sh
+++ ffmpeg/vertical_blur.sh
@@ -1,6 +1,37 @@
 #~/bin/bash
-# create a 9:16 aspect ratio video with blurred sides, 1080x1920 resolution
+# create a 9:16 aspect ratio video with blurred sides
+# defaults to 1080x1920 resolution
 # scale and blur video to produce background
 
-[ $# -ne 2 ] && echo USAGE: $0 infile outfile && exit
-ffmpeg -i "$1" -filter_complex "[0:v]boxblur=40,scale=1080x1920,setsar=1[bg];[0:v]scale=1080:1920:force_original_aspect_ratio=decrease[fg];[bg][fg]overlay=y=(H-h)/2" -c:a copy "$2"
+vertical=1920
+horizontal=1080
+[ $# -lt 2 ] && echo "USAGE: $0 infile outfile [-x horizontal_res -y vertical_res]" && exit
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+	key="$1"
+
+	case $key in
+		-x|--horizontal)
+			horizontal="$2"
+			shift # past argument
+			shift # past value
+			;;
+		-y|--vertical)
+			vertical="$2"
+			shift # past argument
+			shift # past value
+			;;
+		*)    # unknown option
+			POSITIONAL+=("$1") # save it in an array for later
+			shift # past argument
+			;;
+	esac
+done
+input="${POSITIONAL[0]}"
+output="${POSITIONAL[1]}"
+
+ffmpeg -i "$input" -filter_complex "[0:v]boxblur=40,scale=${horizontal}x${vertical},
+	setsar=1[bg];[0:v]scale=${horizontal}:${vertical}:force_original_aspect_ratio=decrease[fg];\
+	[bg][fg]overlay=y=(H-h)/2"\
+	-c:a copy "$output"
