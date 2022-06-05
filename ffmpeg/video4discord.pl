9eb9faa53ce1c73590785b8a84a8246516d9aeb1
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2022-06-05 18:40:38 +0200

    video4discord: accurately take muxing overhead into account

--- ffmpeg/video4discord.pl
+++ ffmpeg/video4discord.pl
@@ -22,7 +22,8 @@ GetOptions(
 die "missing -i <INPUT_FILE>\n" unless $input_file;
 
 my $duration = `ffprobe -v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1  "$input_file"`;
-my $total_bitrate = ($target_filesize * ((100 - $muxing_overhead)/100) * 8192) / $duration;
-my $video_bitrate = int($total_bitrate - $audio_bitrate);
+my $mux = $muxing_overhead / 100;
+my $total_filesize = $target_filesize * 8192;
+my $video_bitrate = (($total_filesize / $duration) - ($mux + 1) * $audio_bitrate)/ ($mux + 1);
 
 `ffmpeg -y -i "$input_file" -c:v libx264 -b:v ${video_bitrate}k -pass 1 -vf "scale=iw/${div}:-1" -vsync cfr -f null /dev/null && ffmpeg -y -i "$input_file" -c:v libx264 -b:v ${video_bitrate}k -pass 2 -vf "scale=iw/${div}:-1" -c:a libopus -b:a ${audio_bitrate}k "$output_file"`;
