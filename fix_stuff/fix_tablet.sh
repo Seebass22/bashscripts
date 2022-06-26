dfc14baef1c8b6964cb9c991cd8871e6b55181f4
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2022-06-26 19:52:34 +0200

    fix fix_tablet.sh

--- fix_stuff/fix_tablet.sh
+++ fix_stuff/fix_tablet.sh
@@ -4,5 +4,5 @@
 
 # xinput | grep -E '\<Pen\>'
 pen="$(xinput | grep -E '\bPen\b' | cut -d'=' -f2 | cut -f1)"
-echo "xinput map-to-output $pen HDMI-4"
-xinput map-to-output $pen HDMI-4
+echo "xinput map-to-output $pen DisplayPort-2"
+xinput map-to-output $pen DisplayPort-2
