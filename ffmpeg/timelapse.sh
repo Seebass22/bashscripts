b86aec35404e8b52bfb274e6e11961bf39655b9c
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2022-07-14 22:09:31 +0200

    timelapse.sh: add --speed option

--- ffmpeg/timelapse.sh
+++ ffmpeg/timelapse.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-[ $# -ne 2 ] && echo USAGE: $0 infile outfile && exit
+[ $# -lt 2 ] && echo USAGE: $0 infile outfile [-t speed-multiplier] && exit
 speed=64
-ffmpeg -i "$1" -r 60 -filter:v "setpts=(1/$speed)*PTS" -vcodec libx264 -an -crf 18 "$2"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+	key="$1"
+
+	case $key in
+		-t|--speed)
+			speed="$2"
+			shift # past argument
+			shift # past value
+			;;
+		*)    # unknown option
+			POSITIONAL+=("$1") # save it in an array for later
+			shift # past argument
+			;;
+	esac
+done
+
+input="${POSITIONAL[0]}"
+output="${POSITIONAL[1]}"
+ffmpeg -i "$input" -r 60 -filter:v "setpts=(1/$speed)*PTS" -vcodec libx264 -an -crf 18 "$output"
