b90cbb19707d4d53104b8153584cabc6c7d73b18
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2022-08-06 14:40:07 +0200

    LaTeX history: refactor, optionally annotate date

--- latex_screenshots.sh
+++ latex_screenshots.sh
@@ -6,6 +6,7 @@ outputdir=summary_output
 pagelayout=4x2
 density=100
 delay=100
+annotate_date=false
 
 compile(){
 	latexmk -pdf -halt-on-error "$1"
@@ -17,12 +18,22 @@ compile(){
 
 # create pdf with multiple pages per pdf page
 summarize(){
-	pdfjam --nup $pagelayout "$1" --outfile "${1%.pdf}_summary.pdf" --landscape
+	pdfjam --nup $pagelayout "$1" --outfile "$2" --landscape
 	rm -f "$1"
 }
 
 convert_to_png(){
-	convert -background white -alpha remove -alpha off -density $density "$1" "${1%.pdf}.png"
+	convert -background white -alpha remove -alpha off -density $density "$1" "$2"
+}
+
+annotate_date() {
+	date="$(git show -s --format=%cs HEAD)"
+
+	mv "$1" "${1}.orig.png"
+	convert "${1}.orig.png"  -gravity south  -pointsize 50 -stroke '#000C' \
+		-strokewidth 2 -annotate 0  "$date" -stroke  none \
+		-fill white -annotate 0 "$date" "$1"
+	rm -f "${1}.orig.png"
 }
 
 POSITIONAL=()
@@ -60,6 +71,11 @@ while [[ $# -gt 0 ]]; do
 			shift # past argument
 			shift # past value
 			;;
+		-a|--annotate-date)
+			annotate_date="true"
+			shift # past argument
+			shift # past value
+			;;
 		*)    # unknown option
 			POSITIONAL+=("$1") # save it in an array for later
 			shift # past argument
@@ -75,11 +91,21 @@ fi
 
 while true; do
 	filename="$(printf '%03d' $i).pdf"
-	if [ ! -f "${outputdir}/${filename%.pdf}_summary.pdf" ]; then
+	summary_pdf="${filename%.pdf}_summary.pdf"
+	summary_png="${summary_pdf%.pdf}.png"
+
+	if [ ! -f "${outputdir}/${summary_png}" ]; then
 		compile "$input"
 		mv "${input%.tex}.pdf" "$filename"
-		summarize "$filename"
-		mv "${filename%.pdf}_summary.pdf" "$outputdir"
+		summarize "$filename" "$summary_pdf"
+		convert_to_png "$summary_pdf" "$summary_png"
+
+		if [ "$annotate_date" == "true" ]; then
+			annotate_date "$summary_png"
+		fi
+
+		rm -f "$summary_pdf"
+		mv "$summary_png" "$outputdir"
 	fi
 
 	# checkout parent commit if exists
@@ -93,15 +119,8 @@ while true; do
 	latexmk -C
 done
 
-# convert all to png
-cd "$outputdir"
-for f in *.pdf; do
-	if [ ! -f "${f%.pdf}.png" ]; then
-		convert_to_png "$f"
-	fi
-done
-
 # create final gif
+cd "$outputdir"
 convert -delay $delay -loop 0 *.png final.gif
 if [ -n "$pad_duration" ]; then
 	ffmpeg -y -i final.gif -vf tpad=stop_mode=clone:stop_duration=$pad_duration final_padded.mp4
